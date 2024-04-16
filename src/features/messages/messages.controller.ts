63fe15d19b832d3e7184b07d72e9cf8251003b36
Kristian <kottes.kristian@gmail.com>
2024-04-16 22:30:24 +0300

    fixed issues

--- src/features/messages/messages.controller.ts
+++ src/features/messages/messages.controller.ts
@@ -7,7 +7,7 @@ export class MessagesController {
   constructor(private readonly messagesService: MessagesService) {}
 
   @Get(':gameId')
-  getMessages(@Param() { game_id }: { game_id: string }) {
-    return this.messagesService.getMessages(game_id);
+  getMessages(@Param() { gameId }: { gameId: string }) {
+    return this.messagesService.getMessages(gameId);
   }
 }
